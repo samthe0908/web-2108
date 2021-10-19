c955021ee73b7577a7f628d7271f7f528d9b1aea
73samthe <stheerawat0908@gmail.com>
2021-10-19 12:31:35 +0200

    Create script.js

---
+++ programmering1/exercises/2021-10-12/js/script.js
@@ -0,0 +1,6 @@
+function newGuess(){
+    let input = (document.getElementById(elementid 'my-guess').value)
+   console.log (input)
+
+    document .getElementById(elementid'guess-output').inneHTML= input
+    }

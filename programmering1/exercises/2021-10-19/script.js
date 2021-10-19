0ccac12bd1d25bc0f88145413227d70c295218d6
73samthe <stheerawat0908@gmail.com>
2021-10-19 12:31:11 +0200

    Create script.js

---
+++ programmering1/exercises/2021-10-19/script.js
@@ -0,0 +1,11 @@
+function nameButton() {
+    let name = document.getElementById('inputName').value
+    document.getElementById('showName').innerText = name
+
+}
+function updateNameButton(){
+    document.getElementById('inputName').value='name'
+}
+
+
+

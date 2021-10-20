067353f1da03fec684bdbc0ee76b953c68e6e650
73samthe <stheerawat0908@gmail.com>
2021-10-20 23:50:25 +0200

    Create script.js
    function på dropdown

---
+++ programmering1/assignment/script.js
@@ -0,0 +1,10 @@
+// test function från lektion
+function checkOutDropdown() {
+    document.getElementById('checkOut')
+        .classList.toggle('show')
+}
+
+function closeMenu() {
+    document.getElementById('checkOut')
+        .classList.remove('show')
+}

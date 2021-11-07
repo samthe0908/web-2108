e8e5da423b774c2745c6f2c9b8726269c32fec58
73samthe <stheerawat0908@gmail.com>
2021-11-07 23:57:22 +0100

    Create script.js

---
+++ programmering1/assignment/js/script.js
@@ -0,0 +1,13 @@
+function openMenu() {
+    document.getElementById('checkOut')
+        .classList.toggle('show')
+}
+function closeMenu() {
+    document.getElementById('checkOut')
+        .classList.remove('show')
+}
+function payment(){
+    alert('Tack för din beställning')
+}
+
+

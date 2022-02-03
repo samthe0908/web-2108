8ffc49ca3fa9dee8f986bec3fd039ea262d30bd3
73samthe <stheerawat0908@gmail.com>
2022-02-03 22:49:34 +0100

    Update Navbar.js
    added cartPic

--- programmering2/assignment/shoppingcart/src/components/Navbar.js
+++ programmering2/assignment/shoppingcart/src/components/Navbar.js
@@ -1,21 +1,37 @@
 
 import '../utils/global/css/Navbar.css'
+import {Link} from "react-router-dom";
 import logo from '../img/mycat.jpg'
-
+import CartPic from '../img/shopping-cart-2351.png'
 export default function Navbar() {
     return (
             <nav className='Navbar'>
-                <div className='ShopName'>
-                    <h1> ALL FOR CATS </h1>
-                </div>
-
-                <ul className='Nav-item'>
-                    <li className='Nav-list '>HOME</li>
-                    <li className='Nav-list ' >FODER</li>
-                    <li className='Nav-list '>ÖVRIG</li>
-                    <li className='Nav-list '>KONTAKT</li>
-                </ul>
+                <div>
                 <img className='Logo' src={logo} alt="logo"/>
+                </div>
+                <div className='Nav-list '>
+                <ul>
+                    <li>
+                        <Link to='/'>HOME</Link>
+                        </li>
+                    <li>
+                        <Link to='/'>FODER</Link>
+                        </li>
+                    <li>
+                        <Link to='/'>ÖVRIG</Link>
+                        </li>
+                    <li>
+                        <Link to='/'>KONTAKT</Link>
+                        </li>
+                </ul>
+                </div>
+                <div className='ShopName'>
+                    <h1 className='Text'> ALL FOR CATS </h1>
+                        <Link to='/'>
+                            <img className='CartPic' src={CartPic} alt="cartPic"/>
+                    </Link>
+
+                </div>
 
          </nav>
     )

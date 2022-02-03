d6333b39231eab0c05356acbc4b5ac8c734c1df1
73samthe <stheerawat0908@gmail.com>
2022-02-03 22:57:10 +0100

    Create Products.js
    skapa product data sida

---
+++ programmering2/assignment/shoppingcart/src/components/Products.js
@@ -0,0 +1,10 @@
+const product = {
+    productItems:[
+    {
+        id: '1',
+        name: 'Miamor Cat Snack 24X15g',
+        price: 65,
+        image:<img src={require('../img/foder_catsnack.jpg')}  alt='catsnack'/>
+    },
+    ]
+}

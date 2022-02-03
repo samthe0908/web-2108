444c1728ee6130a81aff58acae2b614e2571a46f
73samthe <stheerawat0908@gmail.com>
2022-02-03 22:49:05 +0100

    Update App.js
    import

--- programmering2/assignment/shoppingcart/src/App.js
+++ programmering2/assignment/shoppingcart/src/App.js
@@ -1,13 +1,17 @@
 import './utils/global/css/App.css';
 import Navbar from "./components/Navbar";
+import {BrowserRouter as Router} from "react-router-dom";
 
 
 export default function App() {
   return (
-      <>
-
+      <div>
+        <Router>
         <Navbar/>
+        </Router>
+      </div>
 
-      </>
+
+
   )
 }

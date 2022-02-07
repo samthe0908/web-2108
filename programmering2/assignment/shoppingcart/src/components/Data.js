9618f8f2015b970432b9ce51a7ce30ae756d9ddc
73samthe <stheerawat0908@gmail.com>
2022-02-07 22:46:22 +0100

    Create Data.js

---
+++ programmering2/assignment/shoppingcart/src/components/Data.js
@@ -0,0 +1,18 @@
+
+const data = {
+    productItems:[
+    {
+        id: '1',
+        name: 'Miamor Cat Snack 24X15g',
+        price: 65,
+        image:<img src={require('../img/foder_catsnack.jpg')}  alt='catsnack'/>
+    },
+        {
+            id: '2',
+            name: 'Product2',
+            price: 80,
+            image:<img src={require('../img/doft_catnip.jpg')}  alt='catsnack'/>
+        },
+    ]
+}
+export default data;

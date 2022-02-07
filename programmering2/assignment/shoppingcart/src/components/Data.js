96911969c46eb8a43458d3882f7e4226d50a7497
73samthe <stheerawat0908@gmail.com>
2022-02-07 23:33:47 +0100

    Update Data.js

--- programmering2/assignment/shoppingcart/src/components/Data.js
+++ programmering2/assignment/shoppingcart/src/components/Data.js
@@ -5,13 +5,13 @@ const data = {
         id: '1',
         name: 'Miamor Cat Snack 24X15g',
         price: 65,
-        image:<img src={require('../img/foder_catsnack.jpg')}  alt='catsnack'/>
+        image:<img className='Image' src={require('../img/foder_catsnack.jpg')}  alt='catsnack'/>
     },
         {
             id: '2',
-            name: 'Product2',
+            name: 'Trixie Kattnip',
             price: 80,
-            image:<img src={require('../img/doft_catnip.jpg')}  alt='catsnack'/>
+            image:<img className='Image' src={require('../img/doft_catnip.jpg')}  alt='catsnack'/>
         },
     ]
 }

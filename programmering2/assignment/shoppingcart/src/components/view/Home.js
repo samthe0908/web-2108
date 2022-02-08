9fd5151f80264e88b834787db097b98cddae8000
73samthe <stheerawat0908@gmail.com>
2022-02-08 02:55:43 +0100

    Create Home.js

---
+++ programmering2/assignment/shoppingcart/src/components/view/Home.js
@@ -0,0 +1,10 @@
+import '../../utils/global/css/home.css'
+
+const Home = () => {
+    return(
+        <div className='HomePage' >
+            welcome till oss
+        </div>
+    )
+}
+export default Home;

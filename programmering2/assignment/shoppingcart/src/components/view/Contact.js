e25e11e2292c8d9419fa17970c0b3e55d9e50440
73samthe <stheerawat0908@gmail.com>
2022-02-08 02:55:30 +0100

    Create Contact.js

---
+++ programmering2/assignment/shoppingcart/src/components/view/Contact.js
@@ -0,0 +1,10 @@
+import '../../utils/global/css/contact.css'
+
+const Contact = () => {
+    return(
+        <div className='ContactPage' >
+            kontakta oss
+        </div>
+    )
+}
+export default Contact;

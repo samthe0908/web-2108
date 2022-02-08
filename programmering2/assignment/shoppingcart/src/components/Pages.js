9b6ec0f20a126daf7b5aecac843c75beb7f25692
73samthe <stheerawat0908@gmail.com>
2022-02-08 02:55:26 +0100

    Update Pages.js

--- programmering2/assignment/shoppingcart/src/components/Pages.js
+++ programmering2/assignment/shoppingcart/src/components/Pages.js
@@ -1,14 +1,20 @@
 import React from "react";
-import Products from "./Products";
+import Products from "./view/Products";
+import Home from "./view/Home";
+import Contact from "./view/Contact";
+import Cart from "./Cart";
 import {Route, Routes} from "react-router-dom";
+
+
 
 const Pages=({cartItems, productItems, setCartItems}) => {
     return(
     <div>
         <Routes>
-            <Route path="/">Home</Route>
+            <Route path="/" element={<Home/>}/>
             <Route path="/products" element={<Products cartItems={cartItems} productItems={productItems} setCartItems={setCartItems}/>} />
-            <Route path="/contact">Home</Route>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/cart" element={<Cart cartItems={cartItems} productItems={productItems} setCartItems={setCartItems}/>} />
         </Routes>
     </div>
     )

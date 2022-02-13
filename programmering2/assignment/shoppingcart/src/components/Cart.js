d7102f6e2d72bab40f5576d4af3393a1c69015b6
73samthe <stheerawat0908@gmail.com>
2022-02-13 02:13:57 +0100

    Update Cart.js

--- programmering2/assignment/shoppingcart/src/components/Cart.js
+++ programmering2/assignment/shoppingcart/src/components/Cart.js
@@ -1,5 +1,6 @@
+import '../utils/global/css/cart.css'
 
-const Cart = ({cartItems, setCartItems}) => {
+const Cart = ({cartItems, setCartItems,}) => {
     const onIncrease = (itemId) => {
         const copy = [...cartItems];
         const itemToUpdate = copy.find(copyItem => copyItem.id == itemId);
@@ -18,17 +19,44 @@ const Cart = ({cartItems, setCartItems}) => {
         const newItems = cartItems.filter(cartItem => cartItem.id != itemId);
         setCartItems(newItems);
     }
-
+    const totalSum = cartItems.reduce(
+        (price, cartItem)=> price + cartItem.quantity * cartItem.price,0
+    );
+    const clearCart = () => {
+        setCartItems([]);
+    }
     return (
-        <div style={{ color: 'red'}}>Cart
-        {cartItems.map(cartItem =>
+        <div className='Cart-container'>
+            <div className='Cart-item-header'> Din varukorg </div>
+            <div className='Clear-cart'>
+                {cartItems.length >=1&&(
+                    <button className='Clear-cart-button' onClick={clearCart}> Töm varukorg</button>
+                )}
+            </div>
+
+            {cartItems.length === 0&& (
+            <div className='Cart-item-empty'> Din varukorg är tom </div>
+            )}
+
             <div>
-                name:{cartItem.name} price:{cartItem.price * cartItem.quantity} quantity:{cartItem.quantity}
-                <button onClick={() => onIncrease(cartItem.id)}>+</button>
-                <button onClick={() => onDecrease(cartItem.id)}>-</button>
-                <button onClick={() => onDelete(cartItem.id)}>x</button>
-            </div>)}
-    </div>);
+                {cartItems.map(cartItem =>
+                    <div className='ItemInCart'>
+                        <div className='Cart-item-image'>{cartItem.image} </div>
+                        <div className='Cart-item-name'>{cartItem.name} </div>
+                        <div className='Cart-item-price'>Pris:{cartItem.price} </div>
+                        <div className='Cart-item-qnt'>Antal:{cartItem.quantity}</div>
+
+                        <button className='BtnInCart' onClick={() => onIncrease(cartItem.id)}>+</button>
+                        <button className='BtnInCart' onClick={() => onDecrease(cartItem.id)}>-</button>
+                        <button className='BtnInCart' onClick={() => onDelete(cartItem.id)}>x</button>
+                        <div className='Cart-item-totalPrice'>Total pris:{cartItem.price * cartItem.quantity} kr</div>
+                    </div>)}
+            </div>
+            <div className='TotalPriceName'> Summan
+                <div className='Cart-item-sum'>{totalSum} Kr</div>
+            </div>
+
+        </div>);
 }
 
 export default Cart;

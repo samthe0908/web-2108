3e4782416ebd910727b50b1f996678ed996c11c6
73samthe <stheerawat0908@gmail.com>
2022-04-22 15:12:06 +0200

    brutit ut componenterna

--- webbserverprogrammering1/assignment/backend/src/server.js
+++ webbserverprogrammering1/assignment/backend/src/server.js
@@ -1,22 +1,10 @@
 import express from 'express'
-import dotenv from "dotenv";
-import cors from "cors";
+import configarations from "./configuration/configarations.js";
+import ApplyMiddlewares from "./configuration/ApplyMiddlewares.js";
 import todoData from "./data.js";
 
-dotenv.config()
-const port = process.env.SERVER_PORT || 8080
-const allowedRequestOrigins = '*'
-const allowedRequestMethods = ['GET', 'POST', 'PUT', 'DELETE']
-
-const cors_options = {
-    origin: allowedRequestOrigins,
-    methods: allowedRequestMethods
-}
-
 const app = express()
-
-app.use(cors(cors_options))
-app.use(express.json())
+ApplyMiddlewares(app)
 
 const getTodoByName = (name) => {
     let object = `Could not find "${ name }" in database`
@@ -29,13 +17,13 @@ const getTodoByName = (name) => {
     return object
 }
 
-const updateTodoByName = (todo, newTodo, name, done) => {
+
+const updateTodoByName = (todo, newTodo, name, newName) => {
     let object = `Could not find "${ name }" in database`
     todoData.forEach(task => {
         if (name === task.name) {
             task.todo = newTodo
-            task.name = name
-            task.done = done
+            task.name = newName
             object = task
             return
         }
@@ -43,6 +31,20 @@ const updateTodoByName = (todo, newTodo, name, done) => {
     return object
 }
 
+const deleteUserByName = (name) => {
+    let text = `User with name: "${name}" `
+
+    for (let i = 0; i < todoData.length; i++) {
+        if (name === todoData[i].name) {
+            text += `was deleted from database!`
+            todoData.splice(i, 1)
+            return text
+        }
+    }
+
+    text += `don't exist in database!`
+    return text
+}
 
 
 app.get('/', (req, res) => {
@@ -59,15 +61,42 @@ app.get('/task/:name', (req, res) => {
     res.status(200).send(responseFromDb)
 })
 
+app.post('/task/', (req, res) => {
+    const { id, todo, name, done} = req.body
+    const newTask = {
+        id: id,
+        todo: todo,
+        name: name,
+        done: done
+    }
+    todoData.push(newTask)
+    res.status(201).send(todoData)
+
+})
+
 app.put('/task/', (req, res) => {
-    const {todo, newTodo, name, done } = req.body
-    const response = updateTodoByName(todo, newTodo, name, done)
+    const {todo, newTodo, name, newName} = req.body
+    const response = updateTodoByName(todo,newTodo,name, newName)
     res.status(202).send(response)
 })
 
-app.listen(port, ()=> {
-    console.log(`server is running om port http://localhost:${port}`)
+app.put('/task/:id', (req, res) => {
+    const id = Number(req.params.id)
+    todoData[id].done = !todoData[id].done
+    res.status(202).send(todoData[id])
+})
+
+app.delete('/task/:name', (req, res) => {
+    const name = req.params.name
+    const responseFromDb = deleteUserByName(name)
+    res.status(200).send(responseFromDb)
 
 })
+configarations.connectToPort(app)
+
+// app.listen(port, ()=> {
+//     console.log(`server is running om port http://localhost:${port}`)
+//
+// })
 
 

9051c2cb3de2c59e42fb7646908a10c11f68395b
73samthe <stheerawat0908@gmail.com>
2022-05-06 23:33:40 +0200

    skapa Configuration.js

---
+++ datalagring/assigmentToDo/server/src/configurations/Configuration.js
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv'
+import Logger from '../utils/Logger.js'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+const port = process.env.SERVER_PORT || 8080
+const environment = process.env.NODE_ENV
+const mongodb_url = process.env.MONGODB_URL
+const dbName = process.env.MONGODB_DB_NAME
+const dbCollection = process.env.MONGODB_COLLECTION
+
+
+function isServerInDevelopmentMode() {
+    if (environment === 'development') {
+        Logger.warn('Server is in development mode!'.toUpperCase())
+    }
+}
+
+const connectToPort = (app) => {
+    app.listen(port, () => {
+        isServerInDevelopmentMode()
+        Logger.info(`Server running on port http://localhost:${ port }`)
+    })
+}
+
+const connectToDatabase = async () => {
+    const uri = mongodb_url + dbName
+
+    try {
+        await mongoose.connect(uri)
+        Logger.info('Successfully connected to database')
+    } catch (error) {
+        Logger.error('Error connecting to database'.toUpperCase(), error)
+        process.exit()
+    }
+}
+
+export default {
+    connectToPort,
+    connectToDatabase
+}

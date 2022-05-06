41db10043292baee7d14912fcc6bd49a82b623bd
73samthe <stheerawat0908@gmail.com>
2022-05-07 00:23:04 +0200

    import AliveRoute

--- datalagring/assigmentToDo/server/src/server.js
+++ datalagring/assigmentToDo/server/src/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import Configuration from "./configurations/Configuration.js";
 import Middlewares from "./middlewares/Middlewares.js";
 import ApplyMiddlewares from "./middlewares/ApplyMiddlewares.js";
+import AliveController from "./controllers/AliveController.js";
+import AliveRoute from "./routes/AliveRoute.js";
 
 const app = express()
 
 ApplyMiddlewares(app)
+
+AliveRoute.routes(app)
 
 
 app.use(Middlewares.notFound)

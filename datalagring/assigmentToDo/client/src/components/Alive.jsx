81eb623fd01e7d07e0c878cc9ef2bf99fc468224
73samthe <stheerawat0908@gmail.com>
2022-05-07 23:00:41 +0200

    skapa API

---
+++ datalagring/assigmentToDo/client/src/components/Alive.jsx
@@ -0,0 +1,32 @@
+import AliveService from "../utils/api/service/AliveService.js";
+import {useState} from "react";
+const Alive = () => {
+    const [data, setData] = useState('')
+
+    function fetchDataFromExternalApi() {
+        AliveService.alive()
+            .then(response => {
+                setData(response.data)
+            })
+            .catch((error) => console.log(error))
+    }
+
+    function displayData(){
+        if (data.length !== 0){
+            return <h3>Response from API "{data}"</h3>
+        }
+    }
+
+    return(
+        <>
+            <article>
+                <h2>Alive</h2>
+                <button onClick={() => fetchDataFromExternalApi()}>Make API call</button>
+                {displayData()}
+            </article>
+        </>
+    )
+
+}
+
+export default Alive
---
+++ datalagring/assigmentToDo/client/src/utils/api/TaskApi.js
@@ -0,0 +1,11 @@
+import Axios from "axios";
+
+const serverUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost'
+const serverPort = process.env.REACT_APP_SERVER_PORT || 8080
+
+const url = `${ serverUrl }:${ serverPort }`
+const TaskApi = Axios.create({
+    baseURL: url
+})
+
+export default TaskApi

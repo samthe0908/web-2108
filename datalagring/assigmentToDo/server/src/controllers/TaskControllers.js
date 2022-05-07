e3a750f09644903d9eb4dde3bf55a29e033679f0
73samthe <stheerawat0908@gmail.com>
2022-05-07 19:23:37 +0200

    funktion updateTask

--- datalagring/assigmentToDo/server/src/controllers/TaskControllers.js
+++ datalagring/assigmentToDo/server/src/controllers/TaskControllers.js
@@ -50,8 +50,81 @@ const showTasks = (req, res) => {
     }
 }
 
+const getTaskById = (req, res) => {
+    try{
+        TaskModel.findById(req.params.id, (error, tasks) => {
+            if (error) {
+                Logger.error(error)
+                res.status(StatusCode.BAD_REQUEST).send({
+                    error: 'Error getting tasks'
+                })
+            } else {
+                Logger.info(tasks)
+                res.status(StatusCode.OK).send(tasks ? tasks : [{
+                    message: `Task with ID '${req.params.id}' not found`
+                }])
+            }
+        })
+    } catch (error){
+        Logger.error(error)
+        res.status(StatusCode.BAD_REQUEST).send({
+            error: 'Error getting Tasks'
+        })
+    }
+}
 
-const getTaskById = (reg, res) => {
+const getTaskByName = (req, res) => {
+    try{
+        TaskModel.find({name: req.params.name}, (error, tasks) => {
+            if (error) {
+                Logger.error(error)
+                res.status(StatusCode.BAD_REQUEST).send({
+                    error: 'Error getting tasks'
+                })
+            } else {
+                Logger.info(tasks)
+                res.status(StatusCode.OK).send(tasks.length > 0 ? tasks : [{
+                    message: `Task with name '${req.params.name}' not found`
+                }])
+            }
+        })
+    } catch (error){
+        Logger.error(error)
+        res.status(StatusCode.BAD_REQUEST).send({
+            error: 'Error getting Tasks'
+        })
+    }
+}
+
+const updateTaskById = async (req, res) => {
+    try {
+        const updatedTask = {
+            name: req.body.name,
+            task: req.body.task
+        }
+        TaskModel.findByIdAndUpdate(req.params.id, updatedTask, {new: true}, (error, task) => {
+                if (error) {
+                    Logger.error(error)
+                    res.status(StatusCode.BAD_REQUEST).send({
+                        error: 'Error updating task with ID' + req.params.id
+                    })
+                } else {
+                    Logger.info(task)
+                    res.status(StatusCode.OK).send(task ? task : {
+                        message: `Task with ID '${req.params.id}' not found`
+                    })
+                }
+        })
+    } catch (error) {
+        Logger.error(error)
+        res.status(StatusCode.BAD_REQUEST).send({
+            error: 'Error updating task'
+        })
+    }
+}
+
+
+const deleteTaskById = (req, res) => {
     try{
 
     } catch (error){
@@ -62,40 +135,7 @@ const getTaskById = (reg, res) => {
     }
 }
 
-const getTaskByName = (reg, res) => {
-    try{
-
-    } catch (error){
-        Logger.error(error)
-        res.status(StatusCode.BAD_REQUEST).send({
-            error: 'Error XXX user'
-        })
-    }
-}
-
-const updateTaskById = (reg, res) => {
-    try{
-
-    } catch (error){
-        Logger.error(error)
-        res.status(StatusCode.BAD_REQUEST).send({
-            error: 'Error XXX user'
-        })
-    }
-}
-
-const deleteTaskById = (reg, res) => {
-    try{
-
-    } catch (error){
-        Logger.error(error)
-        res.status(StatusCode.BAD_REQUEST).send({
-            error: 'Error XXX user'
-        })
-    }
-}
-
-const updateDone = (reg, res) => {
+const updateDone = (req, res) => {
     try{
 
     } catch (error){

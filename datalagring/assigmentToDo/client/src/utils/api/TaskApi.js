671aae61915ade62f03b223ac18a748fa648cb2d
73samthe <stheerawat0908@gmail.com>
2022-05-07 23:35:12 +0200

    Axios

--- datalagring/assigmentToDo/client/src/utils/api/TaskApi.js
+++ datalagring/assigmentToDo/client/src/utils/api/TaskApi.js
@@ -1,8 +1,7 @@
 import Axios from "axios";
 
 const serverUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost'
-const serverPort = process.env.REACT_APP_SERVER_PORT || 8080
-
+const serverPort = process.env.REACT_APP_SERVER_PORT
 const url = `${ serverUrl }:${ serverPort }`
 const TaskApi = Axios.create({
     baseURL: url

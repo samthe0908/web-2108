809ed1eb0f191a15c14c0675c92b2659b047117b
73samthe <stheerawat0908@gmail.com>
2022-05-08 15:39:20 +0200

    CreateTask.jsx

---
+++ datalagring/assigmentToDo/client/src/components/CreateTask.jsx
@@ -0,0 +1,46 @@
+import TaskService from "../utils/api/service/TaskService";
+import {useState} from "react";
+import TaskCard from "./cards/TaskCard";
+
+
+const CreateTask = () => {
+    const [data, setData] = useState([])
+    const [name, setName] = useState('')
+    const [task, setTask] = useState('')
+
+    const sendDataToApi = () => {
+        const newTask = {
+            'name': name,
+            'task': task,
+        }
+        TaskService.createTask(newTask)
+            .then(response => {
+                setData(response.data)
+            })
+            .catch(error => console.log(error))
+    }
+
+    return(
+        <>
+            <article>
+                <h1> Skapa ny todo</h1>
+                <span> namn: </span>
+                <input type="text"
+                       value={name}
+                       onChange={event => setName(event.target.value)}/>
+                <br/>
+                <span>Task:</span>
+                <input type="text"
+                       value={task}
+                       onChange={event => setTask(event.target.value)}/>
+                <br/>
+                <button onClick={sendDataToApi}>Skapa Ny Task </button>
+                {data.name ? <TaskCard name = {data.name}
+                                       task = {data.task}/>:''}
+
+            </article>
+
+        </>
+    )
+}
+export default CreateTask

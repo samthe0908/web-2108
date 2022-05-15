c85afbeacaaa940ffc7a5fe15f3c1317f180ca3f
samthe0908 <stheerawat0908@gmail.com>
2022-05-15 13:10:03 +0200

    fixat

--- datalagring/assigmentToDo/server/src/routes/TaskRoutes.js
+++ datalagring/assigmentToDo/server/src/routes/TaskRoutes.js
@@ -3,17 +3,13 @@ import TaskControllers from "../controllers/TaskControllers.js";
 const routes = (app) => {
 
     app.post ('/task', TaskControllers.createTask)
-
     app.get('/task/all', TaskControllers.showTasks)
     app.get('/task/name/:name', TaskControllers.getTaskByName)
-    // app.get(`/searchTask/:name', TaskControllers.getTaskByName)
-        
     app.get('/task/:id', TaskControllers.getTaskById)
-
     app.put('/task/:id', TaskControllers.updateTaskById)
     app.delete('/task/:id', TaskControllers.deleteTaskById)
     app.put('/task/:id/done', TaskControllers.updateDone)
-   // app.put(updateDone, TaskControllers.updateDone())
+
 
 
 }

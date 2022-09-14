3adbd763f4c3ae9e737c341533a51949b3dd83cc
samthe0908 <stheerawat0908@gmail.com>
2022-09-14 21:00:25 +0200

    created navbar och css

--- examenarbete/relaxspa/src/components/Navbar/Navbar.tsx
+++ examenarbete/relaxspa/src/components/Navbar/Navbar.tsx
@@ -1,24 +1,28 @@
 import {Container, Nav, Navbar as NavbarBs} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
+import logo from "../../img/relaxspalogo.png"
+//import css from "../Navbar/Navbar.module.css"
+
 
 export function Navbar(){
     return(
     <NavbarBs style={{backgroundColor:"#131313"}}>
        <Container>
            <Nav className="me-auto ">
-               <Nav.Link style={{color:"#AE9A63"}} to="/" as ={NavLink}>
-                   HOME
+               <Nav.Link className="navbar-brand" to="/" as ={NavLink}>
+                   <img src={logo} width="100" className="rounded-circle" alt ="logo-img"/>
                </Nav.Link>
-               <Nav.Link style={{color:"#AE9A63"}} to="/Massage" as ={NavLink}>
+               <Nav.Link className="mt-5" style={{color:"#AE9A63"}} to="/Massage" as ={NavLink}>
+
                    MASSAGE
                </Nav.Link>
-               <Nav.Link style={{color:"#AE9A63"}} to="/Behandlingar" as ={NavLink}>
+               <Nav.Link className="mt-5" style={{color:"#AE9A63"}} to="/Behandlingar" as ={NavLink}>
                    BEHANDLINGAR
                </Nav.Link>
-               <Nav.Link style={{color:"#AE9A63"}} to="/" as ={NavLink}>
+               <Nav.Link className="mt-5"  style={{color:"#AE9A63"}} to="/" as ={NavLink}>
                    ERBJUDANDE
                </Nav.Link>
-               <Nav.Link style={{color:"#AE9A63"}} to="/" as ={NavLink}>
+               <Nav.Link className="mt-5" style={{color:"#AE9A63"}} to="/" as ={NavLink}>
                    OM OSS
                </Nav.Link>
            </Nav>

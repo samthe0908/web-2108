c629913e951b457175b39df6f1f703b4841862e4
samthe0908 <stheerawat0908@gmail.com>
2022-09-19 01:25:49 +0200

    fix bootstrap

--- examenarbete/relaxspa/src/components/Footer/Footer.tsx
+++ examenarbete/relaxspa/src/components/Footer/Footer.tsx
@@ -1,43 +1,43 @@
 import logo from "../../img/relaxspalogo1.png";
 import { FaFacebookSquare, FaInstagramSquare  } from "react-icons/fa";
 import css from "./Footer.module.css"
+import {Col, Row} from "react-bootstrap";
 
 
 
 export function Footer(){
     return(
     <footer style={{backgroundColor:"#131313"}}>
-
-        <div className="row">
+        <Row>
             <div className= "col d-grid justify-content-center" >
                 <img src={logo} width="80"  style={{marginTop:"10px", marginBottom:"5px"}} alt ="logo-img"/>
             </div>
-        </div>
-        <div className="row">
+        </Row>
+        <Row>
             <div className="col d-flex justify-content-center ">
                 <a className={css.facebook}  href="#"><FaFacebookSquare/></a>
                 <a className={css.instagram} href="#"><FaInstagramSquare/></a>
             </div>
-        </div>
-        <div className="row">
-            <div className="col"> </div>
-            <div className="col text-center  "  style={{color:"#AE9A63"}}>
+        </Row>
+        <Row>
+            <Col> </Col>
+            <Col className="col text-center  "  style={{color:"#AE9A63"}}>
                     <h5> Kropp & Själ RelaxSpa </h5>
                     <p>gatuadress</p>
                     <p>postnummer ort</p>
                     <p>telnummer</p>
                     <p> email</p>
-            </div>
-            <div className="col text-center " style={{color:"#AE9A63"}}>
+            </Col>
+            <Col className="col text-center " style={{color:"#AE9A63"}}>
                     <h5> Öppettider </h5>
                     <p>måndag - torsdag 09 - 20</p>
                     <p>fredag - lördag  09 - 18</p>
                     <p>söndag och helgdag 11 - 18</p>
                     <p> Se avvikande öppettider HÄR </p>
-            </div>
-            <div className="col"> </div>
+            </Col>
+            <Col> </Col>
 
-        </div>
+        </Row>
     </footer>
     )
 }

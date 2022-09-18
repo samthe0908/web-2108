6ce89016d5110d78c6c138b8319b418b40aa66ac
samthe0908 <stheerawat0908@gmail.com>
2022-09-19 01:28:03 +0200

    fix header

--- examenarbete/relaxspa/src/pages/Massage.tsx
+++ examenarbete/relaxspa/src/pages/Massage.tsx
@@ -1,3 +1,28 @@
+import css from "./Massage.module.css";
+import {Container, Row, Col} from "react-bootstrap";
+import massageItems from "../data/items.json"
+
+
+
+
 export function Massage(){
-    return <h1> Massage </h1>
+    return(
+        <>
+        <header className={css.massage}/>
+            <div className="p-5" style={{backgroundColor:"#D9D9D9"}}>
+                <Container style={{color:"#AE9A63"}}>
+                </Container>
+            </div>
+
+
+            <Row md={2} xs={1} lg={4} className="g-3">
+                {massageItems.map(item=>(
+                    <Col key={item.id}>
+                        <MassagItem{...item}/>
+                    </Col>
+                ))}
+
+            </Row>
+        </>
+    )
 }

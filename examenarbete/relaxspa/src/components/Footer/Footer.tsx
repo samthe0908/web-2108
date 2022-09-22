8cca2a71e8642fee8fe0144b86e34ed46f55ec9d
samthe0908 <stheerawat0908@gmail.com>
2022-09-22 16:21:06 +0200

    add avvikandetider modal

--- examenarbete/relaxspa/src/components/Footer/Footer.tsx
+++ examenarbete/relaxspa/src/components/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import logo from "../../img/relaxspalogo1.png";
 import { FaFacebookSquare, FaInstagramSquare  } from "react-icons/fa";
 import css from "./Footer.module.css"
-import {Col, Row} from "react-bootstrap";
+import {Button, Col, Modal, ModalBody, ModalHeader, ModalTitle, Row} from "react-bootstrap";
+import React, { useState } from 'react';
 
 
 
 export function Footer(){
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
     <footer style={{backgroundColor:"#131313"}}>
         <Row>
@@ -19,25 +24,54 @@ export function Footer(){
                 <a className={css.instagram} href="#"><FaInstagramSquare/></a>
             </div>
         </Row>
-        <Row xs={1} lg={4}>
+        <Row xs={1} lg={4} >
             <Col> </Col>
-            <Col className="col text-center  "  style={{color:"#AE9A63"}}>
+            <Col className="col text-center mb-5"  style={{color:"#AE9A63"}}>
                     <h5> KROPP & SJÄL RELAXSPA </h5>
                     <p>gatuadress</p>
                     <p>postnummer ort</p>
                     <p>telnummer</p>
                     <p> email</p>
             </Col>
-            <Col className="col text-center " style={{color:"#AE9A63"}}>
+            <Col className="col text-center mb-5"  style={{color:"#AE9A63"}}>
                     <h5> ÖPPETTIDER </h5>
                     <p>måndag - torsdag 09 - 20</p>
                     <p>fredag - lördag  09 - 18</p>
                     <p>söndag och helgdag 11 - 18</p>
-                    <p> Se avvikande öppettider HÄR </p>
+                    <Button className="fs-6"
+                            style={{
+                                backgroundColor:"#131313",
+                                color:"#AE9A63",
+                                borderColor:"#AE9A63",
+                            }}
+                                onClick={handleShow}>
+                        se avvikande öppettider här
+                    </Button>
             </Col>
             <Col> </Col>
-
         </Row>
+
+            <Modal
+            size="sm"
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Avvikande öppettider</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p> Nyårsdagen stängt</p>
+                <p> Midsommarafton stängt</p>
+                <p> Midsommardagen stängt</p>
+                <p> Julafton stängt</p>
+            </Modal.Body>
+            <Modal.Footer >
+                <h3>TREVLIG HELG</h3>
+            </Modal.Footer>
+        </Modal>
+
     </footer>
     )
 }

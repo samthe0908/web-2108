eae42f5219f457ed22014b37973f8eaa7932df57
samthe0908 <stheerawat0908@gmail.com>
2022-09-22 17:00:37 +0200

    add behandling page och tillhörande component

---
+++ examenarbete/relaxspa/src/components/BehandlingItem.tsx
@@ -0,0 +1,38 @@
+import {Card, Button} from "react-bootstrap";
+
+type BehandlingItemProp ={
+    id: number
+    namn: string
+    beskrivning:string
+    pris: number
+    image:string
+}
+export function BehandlingItem({id, namn, beskrivning, pris, image}:BehandlingItemProp){
+    return(
+        <Card className="h-100">
+            <Card.Img
+                variant="top"
+                src={image}
+                height="200px"
+                width="500px"
+                style={{objectFit:"cover"}}/>
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="d-flex mb-4">
+                    <span>{namn} </span>
+
+                </Card.Title>
+                <Card.Text className="d-flex flex-column ">
+                    <span>{beskrivning}</span>
+                </Card.Text>
+                <Button className="fs-4"
+                        style={{
+                            backgroundColor:"#AE9A63",
+                            color:"white",
+                            borderColor:"#AE9A63",
+
+                        }}>BOKA</Button>
+
+            </Card.Body>
+        </Card>
+    )
+}
--- examenarbete/relaxspa/src/pages/Behandlingar.tsx
+++ examenarbete/relaxspa/src/pages/Behandlingar.tsx
@@ -1,3 +1,28 @@
-export function Behandlingar(){
-    return <h1> Behandlingar </h1>
+import css from "./Behandling.module.css";
+import {Container, Row, Col, } from "react-bootstrap";
+import behandlingItems from "../data/behandlingItems.json"
+import {BehandlingItem} from "../components/BehandlingItem";
+
+
+
+
+export function Behandlingar() {
+
+    return (
+        <>
+            <header className={css.behandling}/>
+            <div className="p-5" style={{backgroundColor: "#D9D9D9"}}>
+                <Container style={{color: "#AE9A63"}}>
+                    <Row md={2} xs={1} lg={4} >
+                        {behandlingItems.map(item=>(
+                            <Col Key={item.id}>
+                                <BehandlingItem{...item}/>
+                            </Col>
+                        ))}
+
+                    </Row>
+                </Container>
+            </div>
+        </>
+    )
 }

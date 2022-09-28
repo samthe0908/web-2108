22da4e202ef6c8f2aca113ddaa6245784fb04706
samthe0908 <stheerawat0908@gmail.com>
2022-09-28 15:21:34 +0200

    skapat varukorg

--- examenarbete/relaxspa/src/components/Cart.tsx
+++ examenarbete/relaxspa/src/components/Cart.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import {Offcanvas} from "react-bootstrap";
+import {Offcanvas, Stack} from "react-bootstrap";
+import {useCart} from "../context/CartContext";
+import {CartItem} from "./CartItem";
+import erbjudandeItems from "../data/erbjudande.json"
 
-export function Cart(){
+
+
+type CartProps ={
+    isOpen:boolean
+}
+
+export function Cart({ isOpen }: CartProps){
+    const {closeCart, cartItems} = useCart()
     return(
-        <Offcanvas show = {true}>
+        <Offcanvas style={{width:"500px"}} show = {isOpen} onHide={closeCart} placement="end" >
             <Offcanvas.Header closeButton>
-                <Offcanvas.Title> Varukorg </Offcanvas.Title>
+                <Offcanvas.Title> VARUKORG </Offcanvas.Title>
             </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Stack gap={3} >
+                    {cartItems.map(item =>(
+                    <CartItem key={item.id} {...item}/>
+                        ))}
+                    <div className="ms-auto fw-bold fs-5">
+                        Total {""} {""} {(cartItems.reduce((total,cartItem) => {
+                        const item = erbjudandeItems.find(i => i.id === cartItem.id)
+                        return total + (item?.pris || 0) * cartItem.antal
+                    }, 0))} kr.
+                    </div>
+                </Stack>
+            </Offcanvas.Body>
         </Offcanvas>
     )
 }

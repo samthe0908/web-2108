6ea9c2016f1386a4978314205da04bcaf17a1c1b
samthe0908 <stheerawat0908@gmail.com>
2022-10-05 13:54:15 +0200

    skapa searchView

---
+++ examenarbete/relaxspa/src/pages/TreatmentView.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {SearchCard} from "../components/SearchCard";
+
+export function TreatmentView(){
+
+    return(
+        <>
+            <div className="p-5" style={{backgroundColor: "#D9D9D9"}}>
+          <SearchCard/>
+            </div>
+        </>
+
+
+    )
+}

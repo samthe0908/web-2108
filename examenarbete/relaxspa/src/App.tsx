f8ba8bf1a28ef737f071af3090e8b0925a5f3033
samthe0908 <stheerawat0908@gmail.com>
2022-10-05 13:51:32 +0200

    lägg till search

--- examenarbete/relaxspa/src/App.tsx
+++ examenarbete/relaxspa/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
-import {Home} from "./pages/Home"
-import {Massage} from "./pages/Massage"
-import {Behandlingar} from "./pages/Behandlingar"
+import {HomeView} from "./pages/HomeView"
+import {MassageView} from "./pages/MassageView"
+import {FaceTreatmentView} from "./pages/FaceTreatmentView"
 import {MainNavbar} from "./components/Navbar/Navbar"
 import {Footer} from "./components/Footer/Footer";
-import {Erbjudande} from "./pages/Erbjudande";
-import {About} from "./pages/About";
+import {OfferView} from "./pages/OfferView";
+import {AboutView} from "./pages/AboutView";
 import {CartProvider} from "./context/CartContext";
-
+import {TreatmentView} from "./pages/TreatmentView";
 
 
 function App() {
@@ -17,11 +17,12 @@ function App() {
          <CartProvider>
             <MainNavbar/>
                 <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/Massage" element={<Massage/>}/>
-                     <Route path="/Behandlingar" element={<Behandlingar/>}/>
-                    <Route path="/Erbjudande" element={<Erbjudande/>}/>
-                    <Route path="/About" element={<About/>}/>
+                    <Route path="/" element={<HomeView/>}/>
+                    <Route path="/MassageView" element={<MassageView/>}/>
+                     <Route path="/FaceTreatmentView" element={<FaceTreatmentView/>}/>
+                    <Route path="/OfferView" element={<OfferView/>}/>
+                    <Route path="/AboutView" element={<AboutView/>}/>
+                    <Route path="/TreatmentView" element={<TreatmentView/>}/>
                 </Routes>
             <Footer/>
         </CartProvider>

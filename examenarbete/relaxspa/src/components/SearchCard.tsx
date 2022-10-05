b39db2b9ea88d0db82f9618d54387bd8acfca1e2
samthe0908 <stheerawat0908@gmail.com>
2022-10-05 17:18:07 +0200

    skapa sökfunktion

--- examenarbete/relaxspa/src/components/SearchCard.tsx
+++ examenarbete/relaxspa/src/components/SearchCard.tsx
@@ -1,66 +1,48 @@
-import {Card, Row, Col, Button, Form} from "react-bootstrap";
-import treatmentData from "../data/treatment.json"
-import React, {useState} from "react";
+import {Card, Row, Col, Button} from "react-bootstrap";
+import React from "react";
 
+type SearchItemProp ={
+    id: number
+    cat:string
+    name: string
+    desc:string
+    price: number
+    image:string
+}
 
-export function SearchCard(){
+export function SearchCard({id, cat, name, desc, price, image}:SearchItemProp){
 
-    const [searchText, setSearchText ]= useState("")
-    // @ts-ignore
-
-    
-    return(
+       return (
         <div style={{color: "#AE9A63"}}>
-            <Row className="mb-4">
-                <Form>
-                <Form.Control
-                    size="lg"
-                    className="d-fle"
-                    id="searchInput"
-                    type="search"
-                    placeholder= "sök"
-                    aria-label="Search"
-                    onChange={(event) => {
-                        setSearchText(event.target.value)
-                    }}
-                />
-            </Form>
-            </Row>
+
             <Row md={2} xs={1} lg={4} >
-                {treatmentData.filter((item)=> {
-                   if (searchText == ""){
-                       return []
-                   }else if (item.name.toLowerCase().includes(searchText.toLowerCase())){
-                       return item
-                   }
-                }).map((item, key)=> {
-                    return (
-                        <Col key = {key}>
-                            <Card className="h-100">
-                                <Card.Img
-                                    variant="top"
-                                    src={item.image}
-                                    height="200px"
-                                    width="500px"
-                                    style={{objectFit: "cover"}}/>                                                    <Card.Body className="d-flex flex-column">
-                                <Card.Title className="d-flex mb-4">
-                                    <span>{item.name} </span>
-                                </Card.Title>
-                                <Card.Text className="d-flex flex-column ">
-                                    <span>{item.desc}</span>
-                                </Card.Text>
-                                <Button className="fs-4 mt-auto"
-                                        style={{
-                                            backgroundColor: "#AE9A63",
-                                            color: "white",
-                                            borderColor: "#AE9A63",
-                                        }}>BOKA</Button>
-                            </Card.Body>
-                            </Card>
-                        </Col>
-                    )
-                })}
+                <Col >
+                    <Card className="h-100">
+                        <Card.Img
+                            variant="top"
+                            src={image}
+                            height="200px"
+                            width="500px"
+                            style={{objectFit: "cover"}}/>
+                        <Card.Body className="d-flex flex-column">
+                        <Card.Title className="d-flex mb-4">
+                            <span>{name} </span>
+                        </Card.Title>
+                        <Card.Text className="d-flex flex-column ">
+                            <span>{desc}</span>
+                        </Card.Text>
+                        <Button className="fs-4 mt-auto"
+                                style={{
+                                    backgroundColor: "#AE9A63",
+                                    color: "white",
+                                    borderColor: "#AE9A63",
+                                }}>BOKA</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Row>
         </div>
-    )
-}
+    )}
+
+
+
--- examenarbete/relaxspa/src/pages/TreatmentView.tsx
+++ examenarbete/relaxspa/src/pages/TreatmentView.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import css from "./MassageView.module.css";
+import {Col, Container, Form, Row} from "react-bootstrap";
+import treatmentData from "../data/treatment.json"
 import {SearchCard} from "../components/SearchCard";
 
+
 export function TreatmentView(){
+    const [searchText, setSearchText ]= useState("treatmentData")
+    const onSearchChange = (value) => {
+        const newSearchText = treatmentData.filter((searchData)=>
+            searchData.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()) ||
+            searchData.desc.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+        )
+        setSearchText(newSearchText)
+    }
 
     return(
         <>
             <div className="p-5" style={{backgroundColor: "#D9D9D9"}}>
-          <SearchCard/>
+                <Container style={{color: "#AE9A63"}}>
+                    <Row className="mb-4">
+                        <Form>
+                            <Form.Control
+                                size="lg"
+                                className="d-fle"
+                                id="searchInput"
+                                type="search"
+                                placeholder= "sök"
+                                aria-label="Search"
+                                onChange={event => onSearchChange(event.target.value)}
+                            />
+                        </Form>
+                    </Row>
+                    <Row md={2} xs={1} lg={4} >
+                        {searchText.map((item) =>(
+                            <Col>
+                                <SearchCard{...item}/>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
             </div>
         </>
 

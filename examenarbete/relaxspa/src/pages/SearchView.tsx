e6868f5282218041486f3c4c8b307bcb7212da08
samthe0908 <stheerawat0908@gmail.com>
2022-10-06 22:51:36 +0200

    skapat sök funktion

--- examenarbete/relaxspa/src/pages/SearchView.tsx
+++ examenarbete/relaxspa/src/pages/SearchView.tsx
@@ -3,22 +3,26 @@ import {Col, Container, Form, Row} from "react-bootstrap";
 import treatmentData from "../data/treatment.json"
 import {SearchCard} from "../components/SearchCard";
 
-
 export function SearchView(){
-    const [searchText, setSearchText ]= useState("treatmentData")
-    const onSearchChange = (value) => {
+    const [searchText, setSearchText ]= useState("")
+    const [data, setData ]= useState(treatmentData)
+
+
+    const onSearchChange = (value: string) => {
         const newSearchText = treatmentData.filter((searchData)=>
-            searchData.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()) ||
-            searchData.desc.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+            searchData.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+            // searchData.desc.toLocaleLowerCase().includes(value.toLocaleLowerCase())
         )
-        setSearchText(newSearchText)
+        setSearchText(value)
+        setData(newSearchText)
+
     }
 
     return(
         <>
-            <div className="p-5" style={{backgroundColor: "#D9D9D9"}}>
+            <div  style={{backgroundColor: "#D9D9D9"}}>
                 <Container style={{color: "#AE9A63"}}>
-                    <Row className="mb-4">
+                    <Row className="mb-4 mt-1 ">
                         <Form>
                             <Form.Control
                                 size="lg"
@@ -31,8 +35,9 @@ export function SearchView(){
                             />
                         </Form>
                     </Row>
-                    <Row md={2} xs={1} lg={4} >
-                        {searchText.map((item) =>(
+                    <Row xs={1} lg={2} >
+                        {searchText=="" ||
+                            data.map((item) =>(
                             <Col>
                                 <SearchCard{...item}/>
                             </Col>
